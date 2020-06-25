ce98e56db3800833a7e35a21803baa70e0d2accd
wenghim <hock124999@gmail.com>
2020-06-25 12:35:12 +0800

    CHOO WENG HIM DIB2 G2 19WM02543

---
+++ app/src/main/java/com/example/Toast_Challenge/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.Toast_Challenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private int mCount=0;
+    private TextView mShowCount;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount= findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+        Toast toast= Toast.makeText(this,R.string.toast_message,Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void showCount(View view) {
+
+    }
+
+    public void countUp(View view) {
+        ++mCount;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+}
